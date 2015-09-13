39225581fe587eeb521b21902ff008e41b1a19a1
nitin <testingworldnoida@gmail.com>
2015-09-13 08:49:01 +0530

    Initial 2 test cases

---
+++ JavaAutomation/src/testcases/A.java
@@ -0,0 +1,15 @@
+package testcases;
+
+public class A {
+
+	public void hello()
+	{
+		System.out.println("this is hello");
+	}
+	
+	public void display()
+	{
+		System.out.println("Display");
+	}
+	
+}
---
+++ JavaAutomation/src/testcases/B.java
@@ -0,0 +1,11 @@
+package testcases;
+
+public class B {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is Main");
+		
+	}
+	
+}
